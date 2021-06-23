cfd4f5847b5b50c52d8813db03e91f74b3c41887
ignaciofeito <75045298+ignaciofeito@users.noreply.github.com>
2021-06-23 13:52:45 -0300

    Modificaciones a galería

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import { cuadros } from './cuadros.js'
-import { slide } from './slide.js'
+import { createSlide } from './slide.js'
 
 // Render de un slide por producto
 cuadros.forEach(element => {
-    var s = document.createElement('div');
-    s.innerHTML = "<img src='" + element.img + "' style='width:25%' alt='...'>";
-    s.classList.add("carousel-item");
-    document.getElementById("carouselCuadrosSlides").append(s);
+    createSlide(element.img, element.description, element.name);
 })
